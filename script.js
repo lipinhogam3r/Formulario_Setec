0433c40ac65fa969dccda022def5492f81afcd82
mathlopes123 <142631641+mathlopes123@users.noreply.github.com>
2025-09-17 07:08:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -26,12 +26,20 @@ const btnPagamento = document.getElementById('btnPagamento');
 
 
 
-var cpf = document.querySelector("#cpf");
+function mascara(i){
+   
+   var v = i.value;
+   
+   if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+      i.value = v.substring(0, v.length-1);
+      return;
+   }
+   
+   i.setAttribute("maxlength", "14");
+   if (v.length == 3 || v.length == 7) i.value += ".";
+   if (v.length == 11) i.value += "-";
 
-cpf.addEventListener("blur", function(){
-   if(cpf.value) cpf.value = cpf.value.match(/.{1,3}/g).join(".").replace(/\.(?=[^.]*$)/,"-");
-});
-    return v
 }
+
 
 
