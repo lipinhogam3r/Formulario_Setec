256ad8c539550c5c78bdc8e856df11359dff57d3
mathlopes123 <142631641+mathlopes123@users.noreply.github.com>
2025-09-17 07:04:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,11 +25,17 @@ const btnPagamento = document.getElementById('btnPagamento');
     });
 
 
-function cpf(v){
-    v=v.replace(/\D/g,"")                    //Remove tudo o que não é dígito
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-    v=v.replace(/(\d{3})(\d)/,"$1.$2")       //Coloca um ponto entre o terceiro e o quarto dígitos
-                                             //de novo (para o segundo bloco de números)
-    v=v.replace(/(\d{3})(\d{1,2})$/,"$1-$2") //Coloca um hífen entre o terceiro e o quarto dígitos
+ function cpf(v) {
+            // Remove everything that is not a number
+            v = v.replace(/\D/g, "");
+
+            // Add dots and hyphen as the user types
+            v = v.replace(/(\d{3})(\d)/, "$1.$2");           // First dot
+            v = v.replace(/(\d{3})(\d)/, "$1.$2");           // Second dot
+            v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");     // Hyphen
+
+            return v;
+        }
     return v
 }
+
